6705b0807f5352e82e78ae8934a879f0f6a70447
Julián Calderón Almendros <earendil.elrond.arwen@gmail.com>
2025-12-11 23:09:49 +0100

    Refactor test_uint128.cpp to use explicit namespace for uint128_t

--- tests/test_uint128.cpp
+++ tests/test_uint128.cpp
@@ -2,19 +2,17 @@
 #include <cassert>
 #include <iostream>
 
-using namespace append;
-
 void test_constructor_and_equality() {
     std::cout << "Running test: test_constructor_and_equality\n";
-    uint128_t num1(0x1111222233334444, 0x5555666677778888);
-    uint128_t num2(0x1111222233334444, 0x5555666677778888);
+    append::uint128_t num1(0x1111222233334444, 0x5555666677778888);
+    append::uint128_t num2(0x1111222233334444, 0x5555666677778888);
     assert(num1 == num2);
 
-    uint128_t num3(0, 0);
-    uint128_t num4;
+    append::uint128_t num3(0, 0);
+    append::uint128_t num4;
     assert(num3 == num4);
 
-    uint128_t num5(123);
+    append::uint128_t num5(123);
     assert(num5.high == 0 && num5.low == 123);
     std::cout << "Test passed: test_constructor_and_equality\n";
 }
